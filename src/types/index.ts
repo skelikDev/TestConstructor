c247dfc415a720fae9808374a091660e06e28866
Aleksandr Kim <skelikDev@gmail.com>
2021-09-03 11:53:01 +0300

    describedTypes

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+export interface IUser {
+    id: number
+    email: string
+    password: string
+}
+export interface ITest {
+    id: number
+    title: string
+    authorId: number
+    creationData: string
+}
+
+export interface IQuestion {
+    id: number
+    description: string
+    type: TTypeQuestion
+    order: number
+    TestID: number
+}
+
+export interface IAnswer {
+    id: number
+    value: string
+    correct: boolean
+    questionId: number
+}
+
+export interface ICompletedTest {
+    id: number
+    userId: number
+    testId: number
+    right: number
+    wrong: number
+}
+
+export interface IUsersAnswers {
+    id: number
+    userId: number
+    answerId: number
+    value: string
+}
+type TTypeQuestion = 'radio' | 'check' | 'string'
