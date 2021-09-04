4c5b6c476d65072fa055ac6b40331b9ef8f98b32
Aleksandr Kim <skelikDev@gmail.com>
2021-09-04 06:56:51 +0300

    move Loader from app to individual component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Backdrop, CircularProgress, Container, Grid } from '@material-ui/core'
+import { firebaseOctopus } from '../FirebaseOctopus'
+import { observer } from 'mobx-react'
+import { log } from '../env'
 
-const Loader = (): JSX.Element => {
-    return (
-        <Container>
-            <Grid container alignItems="center" justifyContent="center">
-                <Backdrop open>
-                    <CircularProgress />
-                </Backdrop>
-            </Grid>
-        </Container>
-    )
-}
+const Loader = observer((): JSX.Element | null => {
+    log('render', 'Loader')
+    if (firebaseOctopus.loading) {
+        return (
+            <Container>
+                <Grid container alignItems="center" justifyContent="center">
+                    <Backdrop open>
+                        <CircularProgress />
+                    </Backdrop>
+                </Grid>
+            </Container>
+        )
+    } else {
+        return null
+    }
+})
 
 export default Loader
