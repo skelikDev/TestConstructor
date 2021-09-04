f927b9229fa1607322d28872e4e139109dc2a47d
Aleksandr Kim <skelikDev@gmail.com>
2021-09-04 13:05:45 +0300

    Creacte TestsListBlock

---
+++ src/components/TestsPage/TestsList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TestsListView } from './TestsListView'
+
+// interface TestsListProp {
+//   children: React.ReactNode
+// }
+
+export const TestsList = (): JSX.Element => {
+    return <TestsListView />
+}
